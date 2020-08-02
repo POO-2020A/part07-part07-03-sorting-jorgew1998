6f67bb98c7ab4c52f0ae4e3b1d3145a6d27f9642
jorgew1998 <66254573+jorgew1998@users.noreply.github.com>
2020-08-02 14:35:57 -0500

    Update MainProgram.java

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -4,7 +4,7 @@ public class MainProgram {
     public static void main(String[] args) {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
-        Main.sort(array);
+        MainProgram.sort(array);
         
     }
     
